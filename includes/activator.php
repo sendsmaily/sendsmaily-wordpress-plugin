30b9040da09674d96398fa50f1f2bd4b977000e2
Ra Mänd <ram4nd@gmail.com>
2016-02-26 11:33:19 +0200

    Move install function to separate file in includes directory.

---
+++ includes/activator.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Fired during plugin activation
+ *
+ * @since      1.0.0
+ *
+ * @package    Sendsmaily
+ * @subpackage Sendsmaily/includes
+ */
+
+/**
+ * Install database structure (on activation).
+ * @return void
+ */
+function sendsmaily_install() {
+	global $wpdb;
+	require_once( ABSPATH . 'wp-admin' . DS . 'includes' . DS . 'upgrade.php' );
+	$charset_collate = $wpdb->get_charset_collate();
+
+	// Create database table - settings.
+	$table_name = $wpdb->prefix . 'sendsmaily_config';
+	$query = $wpdb->prepare( 'SHOW TABLES LIKE %s', $wpdb->esc_like( $table_name ) );
+	if ( ! $wpdb->get_var( $query ) ) {
+		$sql = "CREATE TABLE `$table_name` (
+			`key` VARCHAR(128) NOT NULL,
+			`domain` VARCHAR(255) NOT NULL,
+			`autoresponder` INT(16) NOT NULL,
+			`success_url` TEXT NOT NULL,
+			`failure_url` TEXT NOT NULL,
+			`form` TEXT NOT NULL,
+			`is_advanced` TINYINT(1) NOT NULL,
+			PRIMARY KEY(`key`)
+		) $charset_collate;";
+		$wpdb->query( $sql );
+	}
+
+	// Create database table - autoresponders.
+	$table_name = $wpdb->prefix . 'sendsmaily_autoresp';
+	$query = $wpdb->prepare( 'SHOW TABLES LIKE %s', $wpdb->esc_like( $table_name ) );
+	if ( ! $wpdb->get_var( $query ) ) {
+		$sql = trim( preg_replace( '/\s\s+/', ' ', "CREATE TABLE `$table_name` (
+			`id` INT(16) NOT NULL,
+			`title` VARCHAR(255) NOT NULL,
+			PRIMARY KEY (`id`)
+		) $charset_collate;" ) );
+		$wpdb->query( $sql );
+	}
+}
--- sendsmaily.php
+++ sendsmaily.php
@@ -15,6 +15,11 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
 define( 'SS_PLUGIN_VERSION', '0.9.1' );
 
 define( 'BP', dirname( __FILE__ ) );
@@ -29,6 +34,11 @@ define( 'SS_PLUGIN_NAME', $directory );
 
 define( 'SS_PLUGIN_URL', plugins_url( '', __FILE__ ) );
 
+define( 'SS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+
+require_once( SS_PLUGIN_PATH . 'includes/activator.php' );
+register_activation_hook( __FILE__, 'sendsmaily_install' );
+
 /**
  * Initialize.
  * @return void
@@ -38,46 +48,6 @@ function sendsmaily_init() {
 	wp_enqueue_script( 'sendsmaily', SS_PLUGIN_URL . '/js/default.js', false, SS_PLUGIN_VERSION, true );
 }
 add_action( 'init', 'sendsmaily_init' );
-
-/**
- * Install database structure (on activation).
- * @return void
- */
-function sendsmaily_install() {
-	global $wpdb;
-	require_once( ABSPATH . 'wp-admin' . DS . 'includes' . DS . 'upgrade.php' );
-	$charset_collate = $wpdb->get_charset_collate();
-
-	// Create database table - settings.
-	$table_name = $wpdb->prefix . 'sendsmaily_config';
-	$query = $wpdb->prepare( 'SHOW TABLES LIKE %s', $wpdb->esc_like( $table_name ) );
-	if ( ! $wpdb->get_var( $query ) ) {
-		$sql = "CREATE TABLE `$table_name` (
-			`key` VARCHAR(128) NOT NULL,
-			`domain` VARCHAR(255) NOT NULL,
-			`autoresponder` INT(16) NOT NULL,
-			`success_url` TEXT NOT NULL,
-			`failure_url` TEXT NOT NULL,
-			`form` TEXT NOT NULL,
-			`is_advanced` TINYINT(1) NOT NULL,
-			PRIMARY KEY(`key`, `domain`)
-		) $charset_collate;";
-		$wpdb->query( $sql );
-	}
-
-	// Create database table - autoresponders.
-	$table_name = $wpdb->prefix . 'sendsmaily_autoresp';
-	$query = $wpdb->prepare( 'SHOW TABLES LIKE %s', $wpdb->esc_like( $table_name ) );
-	if ( ! $wpdb->get_var( $query ) ) {
-		$sql = trim( preg_replace( '/\s\s+/', ' ', "CREATE TABLE `$table_name` (
-			`id` INT(16) NOT NULL,
-			`title` VARCHAR(255) NOT NULL,
-			PRIMARY KEY (`id`)
-		) $charset_collate;" ) );
-		$wpdb->query( $sql );
-	}
-}
-register_activation_hook( __FILE__, 'sendsmaily_install' );
 
 /**
  * Add sidebar widget.
