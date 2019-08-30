94c1f4f212e6489d69f09f02589183f0a3076862
Kaarel <kaarel.rooparg@gmail.com>
2019-08-30 13:22:19 +0300

    Optimize table creation and add dbDelta

--- includes/activator.php
+++ includes/activator.php
@@ -17,41 +17,34 @@ function sendsmaily_install() {
 	$charset_collate = $wpdb->get_charset_collate();
 
 	// Create database table - settings.
-	$table_name = esc_sql( $wpdb->prefix . 'sendsmaily_config' );
-	$query = "SHOW TABLES LIKE '$table_name'";
-	if ( ! $wpdb->get_var( $query ) ) {
-		$sql = trim(
-			preg_replace(
-				'/\s\s+/',
-				' ',
-				"CREATE TABLE `$table_name` (
-				`key` VARCHAR(128) NOT NULL,
-				`domain` VARCHAR(255) NOT NULL,
-				`autoresponder` INT(16) NOT NULL,
-				`form` TEXT NOT NULL,
-				`is_advanced` TINYINT(1) NOT NULL,
-				PRIMARY KEY(`key`)
-				) $charset_collate;"
-			)
-		);
-		$wpdb->query( $sql );
+	$table_name            = esc_sql( $wpdb->prefix . 'sendsmaily_config' );
+	$settings_table_exists = $wpdb->get_var(
+		$wpdb->prepare(
+			'SHOW TABLES LIKE %s',
+			$table_name
+		)
+	);
+	if ( ! $settings_table_exists ) {
+		$sql = "CREATE TABLE $table_name ( " .
+				'`key` VARCHAR(128) NOT NULL, ' .
+				'domain VARCHAR(255) NOT NULL, ' .
+				'autoresponder INT(16) NOT NULL, ' .
+				'form TEXT NOT NULL, ' .
+				'is_advanced TINYINT(1) NOT NULL, ' .
+				'PRIMARY KEY(`key`) ' .
+				") $charset_collate;";
+		$wpdb->query( $sql ); // Can't use prepare - makes quotes around values.
+		// TODO: Switch to dbDelta().
+		// There is a problem with reserved "key" column name and can't use backticks in dbDelta function.
+		// Should change column name to something else.
 	}
 
 	// Create database table - autoresponders.
 	$table_name = esc_sql( $wpdb->prefix . 'sendsmaily_autoresp' );
-	$query = "SHOW TABLES LIKE '$table_name'";
-	if ( ! $wpdb->get_var( $query ) ) {
-		$sql = trim(
-			preg_replace(
-				'/\s\s+/',
-				' ',
-				"CREATE TABLE `$table_name` (
-				`id` INT(16) NOT NULL,
-				`title` VARCHAR(255) NOT NULL,
-				PRIMARY KEY (`id`)
-				) $charset_collate;"
-			)
-		);
-		$wpdb->query( $sql );
-	}
+	$sql        = "CREATE TABLE $table_name (
+				id int(16) NOT NULL,
+				title varchar(255) NOT NULL,
+				PRIMARY KEY  (id)
+			) $charset_collate;";
+	dbDelta( $sql );
 }
