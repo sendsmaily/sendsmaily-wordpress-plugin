e165854a7fd994bed35503952691731dadd7e21a
TomAbel <tomkristian.abel@tptlive.ee>
2019-10-08 13:47:11 +0300

    Smaily option was leftover after uninstall, this removes it

--- uninstall.php
+++ uninstall.php
@@ -8,3 +8,5 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 global $wpdb;
 $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}sendsmaily_autoresp");
 $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}sendsmaily_config");
+
+delete_option("widget_sendsmaily_subscription_widget");
