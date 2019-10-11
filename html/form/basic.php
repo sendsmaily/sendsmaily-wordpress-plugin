f8c5d863cef41bf57771d9ebf8f8c18b11b98603
TomAbel <tomkristian.abel@tptlive.ee>
2019-10-11 19:28:10 +0300

    Rename action name pattern, fix URL query arguments, $redirect_url refactor.
    Renamed nojs action name pattern to be similar with js enabled one.
    Added removing query arguments at start of nojs function.
    extract wp_get_referer into variable and then check the variable value

--- html/form/basic.php
+++ html/form/basic.php
@@ -5,7 +5,7 @@
 	<p class="error" style="padding:15px;background-color:#f2dede;margin:0 0 10px;display:none"></p>
 	<p class="success" style="padding:15px;background-color:#dff0d8;margin:0 0 10px;display:none"><?php echo esc_html__( 'Thank you for subscribing to our newsletter.', 'wp_smaily' ); ?></p>
 	<input type="hidden" name="lang" value="<?php echo ( defined( 'ICL_LANGUAGE_CODE' ) ) ? ICL_LANGUAGE_CODE : ''; ?>" />
-	<input type="hidden" name="action" value="smly">
+	<input type="hidden" name="action" value="smaily_nojs_subscribe_callback">
 	<?php wp_nonce_field( 'smaily_nonce_field', 'nonce', false ); ?>
 	<p><input type="text" name="email" value="" placeholder="<?php echo esc_html__( 'Email', 'wp_smaily' ); ?>" /></p>
 	<?php if ( $this->show_name ) : ?>
