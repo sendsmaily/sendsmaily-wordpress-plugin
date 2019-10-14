9ce243cd9687e040a119741a3f2847cfa599d2d8
TomAbel <tomkristian.abel@tptlive.ee>
2019-10-14 15:26:07 +0300

    Removed redundant code

--- html/form/basic.php
+++ html/form/basic.php
@@ -1,7 +1,4 @@
 <form id="smly" class="container" action="<?php echo admin_url('admin-post.php'); ?>" method="post">
-	<?php if ( $this->api_credentials === '' ) { ?>
-		<p class="no_auth" style="padding:15px;background-color:#f2dede;margin:0 0 10px"><?php echo esc_html__( 'Smaily credentials not validated. Subscription form will not work!', 'wp_smaily'); ?></p>
-	<?php } ?>
 	<p class="error" style="padding:15px;background-color:#f2dede;margin:0 0 10px;display:<?php echo $this->form_has_error ? 'block' : 'none'; ?>"><?php echo esc_html( $this->error_message ); ?></p>
 	<p class="success" style="padding:15px;background-color:#dff0d8;margin:0 0 10px;display:none"><?php echo esc_html__( 'Thank you for subscribing to our newsletter.', 'wp_smaily' ); ?></p>
 	<input type="hidden" name="lang" value="<?php echo ( defined( 'ICL_LANGUAGE_CODE' ) ) ? ICL_LANGUAGE_CODE : ''; ?>" />
