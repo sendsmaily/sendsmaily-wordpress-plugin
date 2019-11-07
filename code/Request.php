3f0575378ac56aff6327fe39468b1c2d9c7bff02
TomAbel <tomkristian.abel@tptlive.ee>
2019-11-07 14:35:11 +0200

    Revert "Remove feature from readme.txt as it isn't valid"
    This reverts commit e16acda1327c2b7821667d9ef27906f352a02eec.

--- code/Request.php
+++ code/Request.php
@@ -1,30 +1,19 @@
 <?php
 
 class Smaily_Plugin_Request {
+	/**
+	 * Remote request url.
+	 *
+	 * @var string
+	 */
+	protected $_url = '';
 
-	protected $_url = NULL;
-
+	/**
+	 * Remote request data.
+	 *
+	 * @var array
+	 */
 	protected $_data = array();
-
-	private $_username = NULL;
-
-	private $_password = NULL;
-
-	public function auth($username, $password) {
-		$this->_username = $username;
-		$this->_password = $password;
-		return $this;
-}
-
-	public function setUrl($url) {
-		$this->_url = $url;
-		return $this;
-    }
-
-	public function setData(array $data) {
-		$this->_data = $data;
-		return $this;
-}
 
 	/**
 	 * Constructor (set request variables).
