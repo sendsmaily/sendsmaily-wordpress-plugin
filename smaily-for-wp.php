c057fcf8bab480713762402a555e7c044357d895
Kaarel <kaarel.rooparg@gmail.com>
2020-06-08 12:09:30 +0300

    Initial shortform implementation

--- smaily-for-wp.php
+++ smaily-for-wp.php
@@ -87,3 +87,73 @@ function smaily_admin_render() {
 	add_submenu_page( 'smaily', 'Newsletter subscription form', 'Form', 'manage_options', SMLY4WP_PLUGIN_PATH, array( $template, 'dispatch' ) );
 }
 add_action( 'admin_menu', 'smaily_admin_render' );
+
+
+/**
+ * Render smaily form using shortcode.
+ *
+ * @param array $atts shortcode attributes.
+ * @return string
+ */
+function smaily_shortcode_render( $atts ) {
+	global $wpdb;
+
+	// Load configuration data.
+	$table_name = esc_sql( $wpdb->prefix . 'smaily_config' );
+	$config = (array) $wpdb->get_row( "SELECT * FROM `$table_name` LIMIT 1" );
+
+	// Parse attributes out of shortcode tag.
+	$shortcode_atts = shortcode_atts(
+		array(
+			'success_url' => get_site_url(),
+			'failure_url' => get_site_url(),
+			'show_name'   => false,
+		),
+		$atts
+	);
+	$config['success_url'] = $shortcode_atts['success_url'];
+	$config['failure_url'] = $shortcode_atts['failure_url'];
+	$config['show_name']   = $shortcode_atts['show_name'];
+
+	// Create admin template.
+	require_once( SMLY4WP_PLUGIN_PATH . '/code/Template.php' );
+	$file     = ( isset( $config['is_advanced'] ) && '1' === $config['is_advanced'] ) ? 'advanced.php' : 'basic.php';
+	$template = new Smaily_Plugin_Template( 'html/form/' . $file );
+	$template->assign( $config );
+	// Display responses on Smaily subscription form.
+	$form_has_response  = false;
+	$form_is_successful = false;
+	$response_message   = null;
+
+	if ( ! isset( $config['api_credentials'] ) || empty( $config['api_credentials'] ) ) {
+		$form_has_response = true;
+		$response_message  = __( 'Smaily credentials not validated. Subscription form will not work!', 'wp_smaily' );
+	} elseif ( isset( $_GET['code'] ) && (int) $_GET['code'] === 101 ) {
+		$form_is_successful = true;
+	} elseif ( isset( $_GET['code'] ) || ! empty( $_GET['code'] ) ) {
+		$form_has_response = true;
+		switch ( (int) $_GET['code'] ) {
+			case 201:
+				$response_message = __( 'Form was not submitted using POST method.', 'wp_smaily' );
+				break;
+			case 204:
+				$response_message = __( 'Input does not contain a recognizable email address.', 'wp_smaily' );
+				break;
+			default:
+				$response_message = __( 'Could not add to subscriber list for an unknown reason. Probably something in Smaily.', 'wp_smaily' );
+				break;
+		}
+	}
+
+	$template->assign(
+		array(
+			'form_has_response'  => $form_has_response,
+			'response_message'   => $response_message,
+			'form_is_successful' => $form_is_successful,
+		)
+	);
+
+	// Render template.
+	return $template->render();
+}
+add_shortcode( 'smaily_newsletter_form', 'smaily_shortcode_render' );
