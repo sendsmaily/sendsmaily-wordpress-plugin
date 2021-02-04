aebc0a5af269502226d3a8b2327e5fc34e8afee5
TomAbel <tomkristianabel@gmail.com>
2021-02-04 11:25:31 +0200

    Upgrade autoresponders only if smaily_config table exists

--- migrations/upgrade-3-0-0.php
+++ migrations/upgrade-3-0-0.php
@@ -11,18 +11,17 @@ function smailyforwp_upgrade_3_0_0() {
 	global $wpdb;
 	// Halt upgrade if no table exists.
 	$table_name = $wpdb->prefix . 'smaily_config';
-	if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) !== $table_name ) {
-		return update_option( 'smailyforwp_db_version', '3.0.0' );
-	}
-	// Get saved autoresponder ID.
-	$autoresponder_id = $wpdb->get_var( "SELECT autoresponder FROM `$table_name` LIMIT 1" );
-	// Get widgets' options.
-	$widget_options = get_option( 'widget_smaily_subscription_widget' );
+	if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) === $table_name ) {
+		// Get saved autoresponder ID.
+		$autoresponder_id = $wpdb->get_var( "SELECT autoresponder FROM `$table_name` LIMIT 1" );
+		// Get widgets' options.
+		$widget_options = get_option( 'widget_smaily_subscription_widget' );
 
-	foreach ( $widget_options as &$widget ) {
-		// Widgets created before 3.0.0 do not have autoresponder value, adding it here.
-		if ( is_array( $widget ) && ! isset( $widget['autoresponder'] ) ) {
-			$widget['autoresponder'] = $autoresponder_id;
+		foreach ( $widget_options as &$widget ) {
+			// Widgets created before 3.0.0 do not have autoresponder value, adding it here.
+			if ( is_array( $widget ) && ! isset( $widget['autoresponder'] ) ) {
+				$widget['autoresponder'] = $autoresponder_id;
+			}
 		}
 	}
 	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}smaily_autoresponders" );
