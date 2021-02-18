59fda73a5f627f5f5b76a2cf4527b0513880341a
TomAbel <tomkristianabel@gmail.com>
2021-02-18 11:05:41 +0200

    Formatting: spacing w/ parenthesis

--- admin/partials/smaily-for-wp-admin-page.php
+++ admin/partials/smaily-for-wp-admin-page.php
@@ -4,7 +4,7 @@
 		<sup id="h2-loader" style="display:none">(<?php echo __( 'Please wait, working...', 'smaily-for-wp' ); ?>)</sup>
 	</h2>
 
-	<form id="form-container" action="<?php echo admin_url('admin-ajax.php'); ?>" method="post">
+	<form id="form-container" action="<?php echo admin_url( 'admin-ajax.php' ); ?>" method="post">
 		<?php echo $this->partial( 'admin/partials/smaily-for-wp-admin-form.php', $this->getVars() ); ?>
 	</form>
 </div>
